6b2fdea8e2f4595e72b17aafca703b0d89a4415e
Daria <mmmao2212@gmail.com>
2022-09-20 21:20:11 +0200

    used arrow function to determine palindrome, used ternary operator to determine max and min, written function for replace '0' to 'zero'

--- main.js
+++ main.js
@@ -1,1 +1,41 @@
-console.log('hello word');
+const check = (palindrome) =>{
+
+  let long = palindrome.length;
+  for (let i = 0; i < (long / 2); i++ ){
+    console.log(i);
+    if (palindrome[i] !== palindrome[long - 1 - i]){
+      return 'false';
+    }
+  }
+  return 'true';
+}
+
+console.log(check('divider'));
+
+console.log('----------');
+
+let a = 89;
+let b = 86;
+
+max = a > b ? 'true' : 'false';
+console.log(max);
+
+console.log('----------');
+
+let c = 8;
+let d = 86;
+
+min = c < d ? 'true' : 'false';
+console.log(min);
+
+console.log('----------');
+
+const arr = ['4', '12', '50', '70', '62', '10', '93', '90', '7', '100'];
+for (let i = 0; i < arr.length; i++){
+  arr[i] = arr[i].replace('0', 'zero');
+}
+
+console.log(arr);
+
+
+
