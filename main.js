eecbe8d29d347c41e5c9141637e9367f3d333283
Daria Romanowska <mmma2212@gmail.com>
2022-10-05 09:33:44 +0200

    written search for replace '0' to 'zero', used arrays.method

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-const palindrome = (word) =>{
+const palindrome = (word) => {
 
-  let long = word.length;
-  for (let i = 0; i < (long / 2); i++ ){
-    console.log(i);
-    if (word[i] !== word[long - 1 - i]){
-      return false;
+    let long = word.length;
+    for (let i = 0; i < (long / 2); i++) {
+        console.log(i);
+        if (word[i] !== word[long - 1 - i]) {
+            return false;
+        }
     }
-  }
-  return true;
+    return true;
 }
 
 console.log(palindrome('redivider'));
@@ -17,29 +17,38 @@ console.log('----------');
 let a = 56;
 let b = 321;
 
-max = a > b ? a : b;
-console.log(max);
+const max = (a, b) => a > b ? a : b;
+console.log(max(a, b));
 
 console.log('----------');
 
 let c = -45;
 let d = 6;
 
-min = c < d ? c : d;
-console.log(min);
+const min = (c, d) => c < d ? c : d;
+console.log(min(c, d));
 
 console.log('----------');
 
-let arr = [4, 12, 50, 70, 62, 10, 93, 90, 7, 100];
+const arr = [4, 12, 50, 70, 62, 10, 93, 90, 7, 100];
 
-for (let elem in arr){
-  if(arr[elem] / 100 === 1) {
-    arr[elem] = (arr[elem]  / 100) + 'zero' + 'zero';
-  } else if (arr[elem] % 10 === 0) {
-    arr[elem] = arr[elem] / 10 + 'zero';
-  }
+const search = (arr) => {
+    let res = [];
+
+    for (let item of arr) {
+        const replacedZero = String(item).replaceAll('0', 'zero');
+
+        if(Number(replacedZero)) {
+            res.push(Number(replacedZero))
+        } else {
+            res.push(replacedZero);
+        }
+    }
+
+    return res;
 }
-console.log(arr);
-console.log('----------');
 
+const result = search(arr)
+console.log(result);
+console.log('----------');
 
