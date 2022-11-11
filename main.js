aa2a8526b2a5e9203e92c6a391b37285f81971a6
Daria <mmmao2212@gmail.com>
2022-11-11 22:51:43 +0100

    written a new function using array method 'reduce' and fixed output of function result

--- main.js
+++ main.js
@@ -1,68 +1,64 @@
 let date = '2020-11-26';
 
 const format = (query) => {
-    let querySplit = query.split('-');
-    let queryReverse = querySplit.reverse();
-    return queryReverse.join('.');
-}
+  let querySplit = query.split('-');
+  let queryReverse = querySplit.reverse();
+  return queryReverse.join('.');
+};
 
 console.log(format(date));
 
-console.log('--------')
+console.log('--------');
 
 const data = [
-    {
-        country: 'Russia',
-        city: 'Saint Petersburg',
-        hotel: 'Hotel Leopold',
-    },
-    {
-        country: 'Spain',
-        city: 'Santa Cruz de Tenerife',
-        hotel: 'Apartment Sunshine',
-    },
-    {
-        country: 'Slowakia',
-        city: 'Vysokie Tatry',
-        hotel: 'Villa Kunerad',
-    },
-    {
-        country: 'Germany',
-        city: 'Berlin',
-        hotel: 'Hostel Friendship',
-    },
-    {
-        country: 'Indonesia',
-        city: 'Bali',
-        hotel: 'Ubud Bali Resort&SPA',
-    },
-    {
-        country: 'Netherlands',
-        city: 'Rotterdam',
-        hotel: 'King Kong Hostel',
-    },
-    {
-        country: 'Marocco',
-        city: 'Ourika',
-        hotel: 'Rokoko Hotel',
-    },
-    {
-        country: 'Germany',
-        city: 'Berlin',
-        hotel: 'Hotel Rehberge Berlin Mitte',
-    },
+  {
+    country: 'Russia',
+    city: 'Saint Petersburg',
+    hotel: 'Hotel Leopold'
+  },
+  {
+    country: 'Spain',
+    city: 'Santa Cruz de Tenerife',
+    hotel: 'Apartment Sunshine'
+  },
+  {
+    country: 'Slowakia',
+    city: 'Vysokie Tatry',
+    hotel: 'Villa Kunerad'
+  },
+  {
+    country: 'Germany',
+    city: 'Berlin',
+    hotel: 'Hostel Friendship'
+  },
+  {
+    country: 'Indonesia',
+    city: 'Bali',
+    hotel: 'Ubud Bali Resort&SPA'
+  },
+  {
+    country: 'Netherlands',
+    city: 'Rotterdam',
+    hotel: 'King Kong Hostel'
+  },
+  {
+    country: 'Marocco',
+    city: 'Ourika',
+    hotel: 'Rokoko Hotel'
+  },
+  {
+    country: 'Germany',
+    city: 'Berlin',
+    hotel: 'Hotel Rehberge Berlin Mitte'
+  }
 ];
 
+let value = 'Germany';
+const search = data.reduce(function(newArr, data) {
+  if (data.country === value || data.city === value || data.hotel === value) {
+    newArr.push(String(data.country) + ', ' + String(data.city) + ', ' + String(data.hotel));
+  }
+  return newArr;
+}, []);
 
-const find = (value) => {
-    return data.filter(el => el.country === value || el.city ===  value || el.hotel === value)
-}
-
-const result = find('Indonesia')
-
-result.forEach(({country, city, hotel}) => {
-    console.log([country, city, hotel])
-})
-
-
-
+console.log(search);
