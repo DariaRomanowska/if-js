cbe2b367167d9159ef1a085769c56f93f18682e8
Daria <mmmao2212@gmail.com>
2022-11-14 22:52:05 +0100

    added tests to functions, but they don't work well

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
 console.log('----------------');
 
-const sum = x => y => x + y;
+export const sum = (x) => (y) => x + y;
 console.log(sum(5)(2));
 
 console.log('----------------');
@@ -9,25 +9,24 @@ const text1 = document.getElementById('text1');
 const text2 = document.getElementById('text2');
 const text3 = document.getElementById('text3');
 
-
 const colors = ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'];
 
-function getColor(item) {
-  if(colors.find(el => el === item)) {
+export function getColor(item) {
+  if (colors.find((el) => el === item)) {
     for (let i = 0; i < colors.length; i++) {
       if (colors[i] === item) {
-        return colors[++i] ?? colors[0]
+        return colors[++i] ?? colors[0];
       }
     }
   } else {
-    return colors[0]
+    return colors[0];
   }
 }
 
 function setColor(element) {
-  element.style.color = getColor(element.style.color)
+  element.style.color = getColor(element.style.color);
 }
 
-text1.addEventListener("click", () => setColor(text1));
-text2.addEventListener("click", () => setColor(text2));
-text3.addEventListener("click", () => setColor(text3));
+text1.addEventListener('click', () => setColor(text1));
+text2.addEventListener('click', () => setColor(text2));
+text3.addEventListener('click', () => setColor(text3));
