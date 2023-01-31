4c8c88c088d6bca6c86d9b1eee2d77509fde603f
Daria Romanowska <mmma2212@gmail.com>
2023-01-31 13:22:52 +0100

    added notCurrentMonth function to calendar

--- main.js
+++ main.js
@@ -56,109 +56,77 @@ function deepEqual(object1, object2) {
 console.log(deepEqual(obj1, obj2));
 console.log(deepEqual(obj1, obj3));
 
-console.log('--------');
+const daysInMonth = 31;
+const dayOfWeek = 1;
 
-console.log('----LESSON-2-----');
-const palindrome = (word) => {
-  let long = word.length;
-  for (let i = 0; i < long / 2; i++) {
-    console.log(i);
-    if (word[i] !== word[long - 1 - i]) {
-      return false;
+const getCalendarMonth = (daysInMonth, dayOfWeek) => {
+  let day = daysInMonth - dayOfWeek + 1;
+  let dates = [];
+  for (let i = 0; i < 5; i++) {
+    const week = [];
+    for (let j = 0; j < 7; j++) {
+      if (dayOfWeek > 7) {
+        return "Error";
+      } else if (daysInMonth < 28) {
+        return "Error";
+      } else if (day > daysInMonth) {
+        day = 1;
+      }
+      week.push(day);
+      day++;
     }
+    dates.push(week);
   }
-  return true;
+  return dates;
 };
 
-console.log(palindrome('redivider'));
+const calendarMonth = getCalendarMonth(daysInMonth, dayOfWeek);
+let daysArray = calendarMonth.flat();
 
-console.log('----------');
-
-let a = 56;
-let b = 321;
-
-const max = (a, b) => (a > b ? a : b);
-console.log(max(a, b));
-
-console.log('----------');
-
-let c = -45;
-let d = 6;
-
-const min = (c, d) => (c < d ? c : d);
-console.log(min(c, d));
-
-console.log('----------');
-
-const arr = [4, 12, 50, 70, 62, 10, 93, 90, 7, 100];
-
-const search = (arr) => {
-  let res = [];
-
-  for (let item of arr) {
-    const replacedZero = String(item).replaceAll('0', 'zero');
-
-    if (Number(replacedZero)) {
-      res.push(Number(replacedZero));
+const notCurrentMonth = (arr) => {
+  const res = [];
+  for (let o = 0; o < arr.length; o++){
+    if(o < 7 && arr[o] > 27) {
+      res.push(true)
+    } else if (o > 28 && arr[o] < 7){
+      res.push(true)
     } else {
-      res.push(replacedZero);
+      res.push(false)
     }
-  }
+  }return res
+}
+const notCurrentMonthRes = notCurrentMonth(daysArray);
 
-  return res;
+let selectedDate = (arr, checkInDate, checkOutDate) => {
+  let resDate = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (checkInDate === arr[i]) {
+      resDate.push(true);
+    } else if (checkInDate > 31) {
+      return `Error day`;
+    } else if (checkOutDate === arr[i] && checkOutDate > checkInDate) {
+      resDate.push(true);
+    } else if (checkOutDate < checkInDate) {
+      return `Error date`;
+    } else resDate.push(false);
+  }
+  return resDate;
 };
 
-const result = search(arr);
-console.log(result);
-console.log('----------');
+let selectedDateRes = selectedDate(daysArray, 6, 13);
 
-let user;
-user = 'John Doe';
-console.log(user);
+const days = [{}];
+const selected = [{}];
+const current = [{}];
 
-let student;
-// eslint-disable-next-line prefer-const
-student = 'Daria Ramanowska';
-console.log(student);
+daysArray.forEach((item) => days.push({ dayOfMonth: item }));
+selectedDateRes.forEach((item) => selected.push({ selectedDay: item }));
+notCurrentMonthRes.forEach((item) => current.push({ notCurrentMonth: item }));
 
-user = student; //redefined data to 'Daria Ramanowska'
-console.log(user);
 
-let test = 1;
-test++;
-
-test = test + '1';
-console.log(test); // '21' string
-
-test = test - 1;
-console.log(test); // 20 number
-
-test = Boolean(test);
-console.log(test); // true
-
-console.log('----------');
-
-const arr1 = [2, 3, 5, 8];
-let res = 1;
-for (const elem of arr1) {
-  res *= elem;
-}
-
-console.log(res);
-
-console.log('----------');
-
-const arr2 = [2, 5, 8, 15, 0, 6, 20, 3];
-
-for (const i of arr2) {
-  if (i > 5 && i < 10) {
-    console.log(i);
-  }
-}
-console.log('----------');
-
-for (const elem of arr2) {
-  if (elem % 3) {
-    console.log(elem);
-  }
-}
+const calendar = days.map((item, index) => ({
+  ...item,
+  ...selected[index],
+  ...current[index],
+}));
+console.log(calendar);
