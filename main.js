9042d1eb52b07b1c59f4ba499f230c4536199aba
Daria Romanowska <mmma2212@gmail.com>
2023-01-31 14:06:32 +0100

    moved the getList method outside of the constructor, returned the final array

--- main.js
+++ main.js
@@ -1,81 +1,3 @@
-const daysInMonth = 31;
-const dayOfWeek = 1;
-
-const getCalendarMonth = (daysInMonth, dayOfWeek) => {
-  let day = daysInMonth - dayOfWeek + 1;
-  let dates = [];
-  for (let i = 0; i < 5; i++) {
-    const week = [];
-    for (let j = 0; j < 7; j++) {
-      if (dayOfWeek > 7) {
-        return "Error";
-      } else if (daysInMonth < 28) {
-        return "Error";
-      } else if (day > daysInMonth) {
-        day = 1;
-      }
-      week.push(day);
-      day++;
-    }
-    dates.push(week);
-  }
-  return dates;
-};
-
-const calendarMonth = getCalendarMonth(daysInMonth, dayOfWeek);
-let daysArray = calendarMonth.flat();
-console.log(daysArray)
-
-const notCurrentMonth = (arr) => {
-  const res = [];
-  for (let o = 0; o < arr.length; o++){
-    if(o < 7 && arr[o] > 27) {
-      res.push(true)
-    } else if (o > 28 && arr[o] < 7){
-        res.push(true)
-    } else {
-      res.push(false)
-    }
-  }return res
-}
-const notCurrentMonthRes = notCurrentMonth(daysArray);
-
-let selectedDate = (arr, checkInDate, checkOutDate) => {
-  let resDate = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (checkInDate === arr[i]) {
-      resDate.push(true);
-    } else if (checkInDate > 31) {
-      return `Error day`;
-    } else if (checkOutDate === arr[i] && checkOutDate > checkInDate) {
-      resDate.push(true);
-    } else if (checkOutDate < checkInDate) {
-      return `Error date`;
-    } else resDate.push(false);
-  }
-  return resDate;
-};
-
-let selectedDateRes = selectedDate(daysArray, 6, 13);
-console.log(selectedDateRes)
-
-const days = [{}];
-const selected = [{}];
-const current = [{}];
-
-daysArray.forEach((item) => days.push({ dayOfMonth: item }));
-selectedDateRes.forEach((item) => selected.push({ selectedDay: item }));
-notCurrentMonthRes.forEach((item) => current.push({ notCurrentMonth: item }));
-
-
-const calendar = days.map((item, index) => ({
-  ...item,
-  ...selected[index],
-  ...current[index],
-}));
-console.log(calendar);
-
-console.log("----LESSON-8----");
 const studentsData = [
   {
     firstName: "Василий",
@@ -129,14 +51,17 @@ class Student extends User {
 
 class Students {
   constructor(studentList) {
-    this.list = studentList.map(
-      ({ firstName, lastName, admissionYear, courseName }) =>
-        new Student(firstName, lastName, admissionYear, courseName)
-    );
+    this.studentList = studentList
   }
 
+  getList(){
+    return this.studentList.map(
+        ({ firstName, lastName, admissionYear, courseName }) =>
+            new Student(firstName, lastName, admissionYear, courseName));
+      }
+
   getInfo() {
-    const arr = this.list
+    return this.getList()
       .sort((a, b) => {
         return a.course - b.course;
       })
@@ -145,10 +70,10 @@ class Students {
           `${student.fullName} -  ${student.courseName} , ${student.course} курс`
       );
 
-    console.log(arr);
   }
 }
 
 const students = new Students(studentsData);
 
-students.getInfo();
+console.log(students.getInfo());
+
