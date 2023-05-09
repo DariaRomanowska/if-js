61202f985bbd829ed61c3c2418cd83a9cf608cb9
DariaRomanowska <mmmao2212@gmail.com>
2023-05-09 14:11:46 +0200

    added filter display (number of adults, number of children and their age, number of rooms)

--- src/js/main.js
+++ src/js/main.js
@@ -1,212 +1,201 @@
-const checkBlock = [
-  {
-    id: 'adult',
-    name: 'Adults',
-    classInc: 'adult-inc',
-    classDec: 'adult-dec',
+const formBlock = document.getElementById('input-bg');
+
+const modalFilter = document.createElement('div');
+modalFilter.setAttribute('id', 'modal-filter');
+modalFilter.classList.add('check--form');
+formBlock.append(modalFilter);
+
+const modalWindow = document.getElementById('showModal');
+let selected = {
+  children: [],
+};
+const counts = {
+  adult: {
+    count: 0,
   },
-  {
-    id: 'child',
-    name: 'Children',
-    classInc: 'child-inc',
-    classDec: 'child-dec',
+  room: {
+    count: 0,
   },
-  {
-    id: 'room',
-    name: 'Rooms',
-    classInc: 'room-inc',
-    classDec: 'room-dec',
+  child: {
+    count: 0,
   },
-];
+};
+const elements = {
+    adults: {
+      identifier: 'adults',
+      label: 'Adults',
+      value: 0,
+      count: 0,
+      values: {
+        min: 0,
+        max: 10,
+      },
+    },
+    rooms: {
+      identifier: 'rooms',
+      label: 'Rooms',
+      value: 0,
+      count: 0,
+      values: {
+        min: 0,
+        max: 10,
+      },
+    },
+    children: {
+      identifier: 'children',
+      label: 'Children',
+      value: 0,
+      count: 0,
+      values: {
+        min: 0,
+        max: 10,
+      },
+      events: {
+        onValueUpdate: function (element, div) {
+          if (element.value > 0) {
+            let title = document.createElement('h3');
+            title.innerText =
+              'What is the age of the child you’re travelling with?';
+            title.classList.add('child-text');
+            div.appendChild(title);
+            for (
+              let childIndex = 1;
+              childIndex <= element.value;
+              childIndex++
+            ) {
+              let childrenBlock = document.createElement('div'),
+                select = document.createElement('select');
+              select.setAttribute('data-index', childIndex);
 
+              select.classList.add('child-opt');
+              select.name = `child[${childIndex - 1}]`;
 
-const i = 0;
-let childrenOptionAge = [];
-for( let i = 0; i < 17; ){
-  childrenOptionAge.push(i + 1 + 'years old')
-}
-console.log(childrenOptionAge)
-//   '0 years old',
-//   '1 years old',
-//   '2 years old',
-//   '3 years old',
-//   '4 years old',
-//   '5 years old',
-//   '6 years old',
-//   '7 years old',
-//   '8 years old',
-//   '9 years old',
-//   '10 years old',
-//   '11 years old',
-//   '12 years old',
-//   '13 years old',
-//   '14 years old',
-//   '15 years old',
-//   '16 years old',
-//   '17 years old',
-// ];
+              let chooseChildAgeOption = document.createElement('option');
+              chooseChildAgeOption.innerText = 'Choose child age';
 
-const checkBar = document.getElementById('form-main');
-const barButton = document.getElementById('check-bar');
+              select.append(chooseChildAgeOption);
 
-const checkForm = document.createElement('div');
-checkForm.className = 'check--form';
-checkBar.appendChild(checkForm);
+              for (let i = 0; i < 18; i++) {
+                let option = document.createElement('option');
+                option.classList.add('child-select');
+                option.value = i;
+                option.innerText = `${i} years`;
+                option.setAttribute('data-index', i);
 
-window.addEventListener('load', function () {
-  checkBlock.map((element) => {
-    const checkFormContent = document.createElement('div');
-    checkFormContent.className = 'check--form_content';
-    checkForm.appendChild(checkFormContent);
+                if (selected.children[childIndex] == i) {
+                  option.setAttribute('selected', true);
+                }
 
-    const checkName = document.createElement('p');
-    checkName.className = 'check-name';
-    checkName.innerHTML = element.name;
-    checkFormContent.appendChild(checkName);
+                select.appendChild(option);
+              }
+              select.addEventListener('change', (e) => {
+                let target = e.target;
+                selected.children[target.getAttribute('data-index')] = parseInt(
+                  target.value,
+                );
+              });
+              childrenBlock.appendChild(select);
+              div.append(childrenBlock);
+            }
+          }
+        },
+      },
+    },
+  },
+  container = document.getElementById('modal-filter');
 
-    const checkStepper = document.createElement('div');
-    checkStepper.className = 'check-stepper';
-    checkFormContent.appendChild(checkStepper);
+function render(container, elements) {
+  function buildBlock(element) {
+    let div = document.createElement('div'),
+      divContent = document.createElement('div'),
+      label = document.createElement('label'),
+      input = document.createElement('input'),
+      btnDecrease = document.createElement('button'),
+      btnIncrease = document.createElement('button'),
+      updateValue = function (div) {
+        if (element.hasOwnProperty('events')) {
+          let events = element.events;
 
-    const countQuery = document.createElement('input');
-    countQuery.setAttribute('id', element.id);
-    countQuery.setAttribute('value', '0');
-    countQuery.className = 'check-count';
-    checkStepper.appendChild(countQuery);
-
-    const btnMinus = document.createElement('button');
-    btnMinus.className = 'btn-check-l';
-    btnMinus.type = 'button';
-    btnMinus.innerHTML = '-';
-    btnMinus.classList.add(element.classDec);
-    checkStepper.appendChild(btnMinus);
-
-    const btnPlus = document.createElement('button');
-    btnPlus.className = 'btn-check-r';
-    btnPlus.type = 'button';
-    btnPlus.innerHTML = '+';
-    btnPlus.classList.add(element.classInc);
-    checkStepper.appendChild(btnPlus);
-  });
-
-  document.addEventListener('click', function (e) {
-    const adult = document.getElementById('adult');
-    const adultValue = adult.value;
-    const room = document.getElementById('room');
-    const roomValue = room.value;
-    const child = document.getElementById('child');
-    const childValue = child.value;
-    if (e.target.classList.contains('adult-inc')) {
-      if (Number(adultValue) + 1 > 30) {
-        return;
-      }
-      ++adult.value;
-    } else if (e.target.classList.contains('adult-dec')) {
-      if (Number(adultValue) - 1 < 0) {
-        return;
-      }
-      --adult.value;
-    }
-    if (e.target.classList.contains('room-inc')) {
-      if (Number(roomValue) + 1 > 30) {
-        return;
-      }
-      ++room.value;
-    } else if (e.target.classList.contains('room-dec')) {
-      if (Number(roomValue) - 1 < 0) {
-        return;
-      }
-      --room.value;
-    }
-    if (e.target.classList.contains('child-inc')) {
-      if (Number(childValue) + 1 > 10) {
-        return;
-      }
-      ++child.value;
-    } else if (e.target.classList.contains('child-dec')) {
-      if (Number(childValue) - 1 < 0) {
-        return;
-      }
-      --child.value;
-    }
-  });
-
-  const childBlockAge = document.createElement('div');
-  childBlockAge.className = 'child-block';
-  checkForm.append(childBlockAge);
-
-  const childQuest = document.createElement('p');
-  childQuest.className = 'child-text';
-  childQuest.innerHTML = 'What is the age of the child you’re travelling with?';
-  childBlockAge.appendChild(childQuest);
-
-  const divSelect = document.createElement('div');
-  divSelect.setAttribute('id', 'select-block');
-  divSelect.className = 'child-select-age';
-
-  document.addEventListener('click', function (event) {
-    const children = document.getElementById('child');
-    let value = children.value;
-    if (Number(value) > 0) {
-      childBlockAge.classList.add('show-child');
-    } else {
-      childBlockAge.classList.remove('show-child');
-    }
-
-    function addSelect() {
-      const childSelect = document.createElement('SELECT');
-      childSelect.className = 'child-select';
-      childBlockAge.appendChild(divSelect);
-      divSelect.appendChild(childSelect);
-      for (const i in childOldArray) {
-        const childOpt = document.createElement('option');
-        childOpt.className = 'child-opt';
-        childOpt.innerHTML = childOldArray[i];
-        childSelect.appendChild(childOpt);
-      }
-      const currentSelects = document.querySelectorAll(
-        '#select-block .child-select',
-      );
-      console.log(currentSelects);
-      const currentSelectCount = currentSelects.length;
-      console.log(currentSelectCount);
-      if (value !== currentSelectCount) {
-        if (value < currentSelectCount) {
-          divSelect[0].remove();
+          if (events.hasOwnProperty('onValueUpdate')) {
+            events.onValueUpdate(element, container);
+          }
         }
-      } else {
-        return addSelect;
-      }
-    }
-    addSelect();
-  });
+      };
 
-  // function countChildrenAge() {
-  //   const inputChild = document.getElementById('child');
-  //   const value = Number(inputChild.value);
-  //   const currentSelect = document.querySelectorAll('.child-select');
-  //   console.log(currentSelect)
-  //   const currentSelectCount = currentSelect.length;
-  //   const newSelect = document.querySelectorAll('.child-select');
-  //   if (value !== currentSelectCount) {
-  //     if (value > currentSelectCount) {
-  //       divSelect.appendChild(newSelect);
-  //     } else {
-  //       divSelect.removeChild();
-  //     }
-  //   }
-  // }
-  //
-  // countChildrenAge();
-});
+    div.setAttribute('data-div', `${element.identifier}-block`);
+    div.classList.add('check--form_content');
+    divContent.classList.add('check-stepper');
+    label.innerText = element.label;
+    label.classList.add('check-name');
+    input.classList.add('check-count');
+    btnDecrease.innerText = '-';
+    btnIncrease.innerText = '+';
 
-barButton.addEventListener('click', () => {
-  document.querySelector('.check--form').classList.add('show');
-});
+    input.value = element.value;
 
-window.addEventListener('click', (e) => {
-  const withinBoundaries = e.composedPath().includes(checkBar);
-  if (!withinBoundaries) {
-    checkForm.classList.remove('show');
+    btnDecrease.setAttribute('data-update', element.identifier);
+    btnDecrease.setAttribute('type', 'button');
+    btnDecrease.className = 'btn-check-l';
+    btnDecrease.classList.add('active');
+
+    btnIncrease.setAttribute('data-update', element.identifier);
+    btnIncrease.setAttribute('type', 'button');
+    btnIncrease.className = 'btn-check-r';
+    btnIncrease.classList.add('active');
+
+    btnDecrease.addEventListener('click', (e) => {
+      if (element.value <= element.values.min) return;
+
+      elements[e.target.getAttribute('data-update')].value--;
+
+      let counter = document.querySelector(`[data-value='${element.identifier}']`);
+      counter.innerText = [element.value, element.label].join(' ');
+
+      container.innerHtml = '';
+      render(container, elements);
+      updateValue(element, container);
+    });
+
+    btnIncrease.addEventListener('click', (e) => {
+      if (element.value === element.values.max) return;
+
+      elements[e.target.getAttribute('data-update')].value++;
+
+      let counter = document.querySelector(`[data-value='${element.identifier}']`);
+      counter.innerText = [element.value, element.label].join(' ');
+
+      container.innerHtml = '';
+      render(container, elements);
+      updateValue(element, container);
+    });
+
+    if (element.value <= element.values.min)
+      btnDecrease.classList.add('disabled');
+    if (element.value === element.values.max)
+      btnIncrease.classList.add('disabled');
+
+    divContent.append(btnDecrease, input, btnIncrease);
+
+    div.append(label, divContent);
+
+    return div;
   }
+
+  container.innerHTML = '';
+
+  Object.keys(elements).forEach((element) =>
+    container.append(buildBlock(elements[element])),
+  );
+
+}
+
+render(container, elements, counts);
+
+modalWindow.addEventListener('click', () => {
+  modalFilter.classList.add('show-modal');
+});
+
+const bg = document.querySelector('#bg');
+bg.addEventListener('click', () => {
+  modalFilter.classList.remove('show-modal');
 });
